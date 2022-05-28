c0441c053798a8f76a7e6d03d6ef7af60ece0f25
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 13:55:03 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,54 +1,59 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/cf/string.hpp>
 
-TEST_CASE("make_string") {
-  {
-    std::string expected("example");
-    auto actual = pqrs::cf::make_string(CFSTR("example"));
-    REQUIRE(actual != std::nullopt);
-    REQUIRE(*actual == expected);
-  }
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-  {
-    // With Unicode characters
-    std::string expected("example ★★ example");
-    auto actual = pqrs::cf::make_string(CFSTR("example ★★ example"));
-    REQUIRE(actual != std::nullopt);
-    REQUIRE(*actual == expected);
-  }
-
-  REQUIRE(pqrs::cf::make_string(static_cast<CFTypeRef>(nullptr)) == std::nullopt);
-  REQUIRE(pqrs::cf::make_string(static_cast<CFStringRef>(nullptr)) == std::nullopt);
-
-  {
-    if (auto dictionary = CFDictionaryCreateMutable(nullptr,
-                                                    0,
-                                                    &kCFTypeDictionaryKeyCallBacks,
-                                                    &kCFTypeDictionaryValueCallBacks)) {
-      REQUIRE(pqrs::cf::make_string(dictionary) == std::nullopt);
-
-      CFRelease(dictionary);
-    } else {
-      REQUIRE(false);
+  "make_string"_test = [] {
+    {
+      std::string expected("example");
+      auto actual = pqrs::cf::make_string(CFSTR("example"));
+      expect(actual != std::nullopt);
+      expect(*actual == expected);
     }
-  }
-}
 
-TEST_CASE("make_cf_string") {
-  {
-    auto expected = CFSTR("example");
-    auto actual = pqrs::cf::make_cf_string("example");
-    REQUIRE(actual);
-    REQUIRE(CFEqual(*actual, expected));
-  }
+    {
+      // With Unicode characters
+      std::string expected("example ★★ example");
+      auto actual = pqrs::cf::make_string(CFSTR("example ★★ example"));
+      expect(actual != std::nullopt);
+      expect(*actual == expected);
+    }
 
-  {
-    // With Unicode characters
-    auto expected = CFSTR("example ★★ example");
-    auto actual = pqrs::cf::make_cf_string("example ★★ example");
-    REQUIRE(actual);
-    REQUIRE(CFEqual(*actual, expected));
-  }
+    expect(pqrs::cf::make_string(static_cast<CFTypeRef>(nullptr)) == std::nullopt);
+    expect(pqrs::cf::make_string(static_cast<CFStringRef>(nullptr)) == std::nullopt);
+
+    {
+      if (auto dictionary = CFDictionaryCreateMutable(nullptr,
+                                                      0,
+                                                      &kCFTypeDictionaryKeyCallBacks,
+                                                      &kCFTypeDictionaryValueCallBacks)) {
+        expect(pqrs::cf::make_string(dictionary) == std::nullopt);
+
+        CFRelease(dictionary);
+      } else {
+        expect(false);
+      }
+    }
+  };
+
+  "make_cf_string"_test = [] {
+    {
+      auto expected = CFSTR("example");
+      auto actual = pqrs::cf::make_cf_string("example");
+      expect(actual);
+      expect(CFEqual(*actual, expected));
+    }
+
+    {
+      // With Unicode characters
+      auto expected = CFSTR("example ★★ example");
+      auto actual = pqrs::cf::make_cf_string("example ★★ example");
+      expect(actual);
+      expect(CFEqual(*actual, expected));
+    }
+  };
+
+  return 0;
 }
